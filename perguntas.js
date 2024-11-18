85d0d124cf46711cf98f2c2abdf99b06be7b700f
Hellen Veine <141366243+VeineHellen@users.noreply.github.com>
2024-11-18 11:08:55 -0300

    aaaa

--- perguntas.js
+++ perguntas.js
@@ -18,36 +18,36 @@ criaCartao(
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz "a Walesca tem um pinscher raivoso" em Inglês?',
+    '"a Walesca tem um pinscher raivoso" em ingles é "Walesca haves a angryoso pinscher'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz "a Walesca ama Química" em Inglês?',
+    '"a Walesca ama química" em ingles é "Walesca loves Chemistry"'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz "estou indo para a escola" em Inglês?',
+    '"estou indo para a escola" em ingles é "im going to school"'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz "a Hellen ama cachorros" em Inglês?',
+    '"a Hellen ama cachorros" em ingles é "Hellen loves dogs"'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz "como você está?" em Inglês?',
+    '"como você está?" em ingles é how are you?'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz "estou bem" em Inglês?',
+    'Estou bem em ingles é Im fine'
 )
